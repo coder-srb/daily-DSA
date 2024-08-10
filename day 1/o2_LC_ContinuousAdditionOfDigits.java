3cd18d02771c04b20c38241295fc06c848e8230d
SOUBHAGYARANJAN BISOI <149040354+coder-srb@users.noreply.github.com>
2024-08-10 11:51:24 +0530

    Update o2_LC_ContinuousAdditionOfDigits.java
    just added a shortcut trick to find the continuous sum of digits

--- day 1/o2_LC_ContinuousAdditionOfDigits.java
+++ day 1/o2_LC_ContinuousAdditionOfDigits.java
@@ -30,6 +30,13 @@ class Solution2 {
             num = currSum;
         }
         return num;
+
+	// another way
+	/*
+	if(num==0) return 0;
+        else if(num%9==0) return 9;
+        else return num%9; 
+	*/
     }
 }
 
